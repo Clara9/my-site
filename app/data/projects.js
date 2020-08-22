b7268fc892531009a25b35cb030b9db54688435b
Clara9 <ziyuan3@illinois.edu>
2020-08-21 22:40:23 -0500

    Update projects.js

--- app/data/projects.js
+++ app/data/projects.js
@@ -4,14 +4,14 @@ const data = [
     title: 'Private Tracking for COVID-19',
     subtitle: 'CS411 Final Project',
     link: 'https://github.com/Clara9/CS411_Backend',
-    image: '/images/projects/images.jpeg',
+    image: '/public/images/projects/images.jpeg',
     date: 'July - August 2020',
     desc: 'Built Python-Pyramid API endpoints with REST to insert, update and delete items, with SQL queries for MariaDB access.',
   }, {
     title: 'Virus Spread Data Analysis',
     subtitle: 'RMDS Lab Data Projects',
     link: 'https://grmds.org/project-coronavirus',
-    image: '/images/projects/download.jpeg',
+    image: '/public/images/projects/download.jpeg',
     date: 'March 2020',
     desc: 'Extracted and visualized data from provincial and city datasets in China using PCA analysis, with seaborn and sklearn libraries from Python. ' +
     'Cleaned and merged data from hospital dataset, and classify with decision-tree in Python. ' +
@@ -20,7 +20,7 @@ const data = [
     title: 'Budget Savings App',
     subtitle: 'Hackathon Project for 2019 Capital One SWE Summit',
     link: 'https://github.com/Clara9/CapitalOneHackSavings',
-    image: '/images/projects/budget.jpg',
+    image: '/public/images/projects/budget.jpg',
     date: 'August 2019',
     desc: 'Implemented Camera API in Android Studio. ' + 
       'Researched on image-classification and text-recognition with Google Vision API and Tesseract OCR',
@@ -28,7 +28,7 @@ const data = [
     title: 'Personality Analysis',
     subtitle: 'CS126 Final Project',
     link: 'https://github.com/uiuc-sp19-cs126/final-project-Clara9',
-    image: '/images/projects/personality.png',
+    image: '/public/images/projects/personality.png',
     date: 'April - May 2019',
     desc: 'Preprocessed datasets of Amazon reviews in Python into personality types using MBTI scale, with sent2vec and word embedding'
      + 'Classified and trained the preprocessed data using KNN algorithms, written in C++',
@@ -37,7 +37,7 @@ const data = [
     title: 'NPS(National Park Service) Web Application',
     subtitle: 'Application Project for 2019 Capital One SWE Summit',
     link: 'https://clara9.github.io/NPSWebApp/',
-    image: '/images/projects/nps.jpg',
+    image: '/public/images/projects/nps.jpg',
     date: 'June 2019',
     desc: 'Provided tools to assist users in state and designation filtering, name and keyword search with JavaScript and AJAX. ' + 
     'Listed details about specific visitor centers and displayed alerts, articles, etc. about a selected destination '
@@ -45,7 +45,7 @@ const data = [
     title: 'Android Chat App',
     subtitle: 'CS125 Final Project',
     link: 'https://github.com/Clara9/ChatApp',
-    image: '/images/projects/chat.jpg',
+    image: '/public/images/projects/chat.jpg',
     date: 'November - December 2019',
     desc: 'Stored chat history and authenticated users with Firebase. '
      + 'Designed and implemented UI for chat interface',
@@ -53,7 +53,7 @@ const data = [
     title: 'Campus Safety App',
     subtitle: '2018 Hack Shanghai Project',
     link: 'https://clara9.github.io/CampusSafetyWebApp/',
-    image: '/images/projects/campus.jpg',
+    image: '/public/images/projects/campus.jpg',
     date: 'July 2018',
     desc: 'Detected user location using JavaScript and Google Map API. ' + 
     'Sent texts to emergency contacts with SMS API',
